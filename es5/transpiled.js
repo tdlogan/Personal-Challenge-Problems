716e85a5e9c76facbe96a41d68f7907bf19a04c1
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-07-01 15:02:54 -0700

    [problem] adding new problem string compression

--- es5/transpiled.js
+++ es5/transpiled.js
@@ -1,9 +1,16 @@
-//Given two strings, implement a method that determines if the input string is a permutation of the other
+//Compress a string by creating a method that returns a compressed form the of the given string.
+//If the given string is longer after the compression is attempted, return the original string instead.
+//Example: "aaabbcccc".stringCompression(); -> "a3b2c4"
+//Example: "a".stringCompression(); -> "a" would be returned, not "a1"
+
+//Writing a traditional function declaration here is necessary
+//In Babel 'this' is not attached to window and will transpile into undefined!
+"use strict";
+
+String.prototype.stringCompression = function () {}; //Given two strings, implement a method that determines if the input string is a permutation of the other
 
 //Writing a traditional function declaration here is necessary
 //In Babel this is not attached to window and will transpile into undefined!
-"use strict";
-
 String.prototype.permutationCheck = function (str) {}; //Create a function to determine if a string has all unique characters
 
 var unique = function unique(str) {};
--- es6/module_1/string_compression.js
+++ es6/module_1/string_compression.js
@@ -8,21 +8,3 @@
 String.prototype.stringCompression = function() {
 
 } 
-;//Given two strings, implement a method that determines if the input string is a permutation of the other
-
-//Writing a traditional function declaration here is necessary
-//In Babel this is not attached to window and will transpile into undefined!
-String.prototype.permutationCheck = function(str) {
-
-} 
-;//Create a function to determine if a string has all unique characters
-
-let unique = (str) => {
-
-}
-
-//EXTRA CREDIT: Implement the unique problem without creating additioanl data structures
-
-let unique_extra = (str) => {
-
-}
