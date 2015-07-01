bf50413257c0dfeded6dcec0e536bc53e8279f4a
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-06-30 17:13:10 -0700

    [solution] finished first part of unique.

--- es6/module_1/unique.js
+++ es6/module_1/unique.js
@@ -1,7 +1,16 @@
 //Create a function to determine if a string has all unique characters
 
 let unique = (str) => {
+  let characters = {};
 
+  for (let i of str) {
+    if (characters[i]) {
+      return false;
+    } else {
+      characters[i] = true;
+    }
+  }
+  return true;
 }
 
 //EXTRA CREDIT: Implement the unique problem without creating additioanl data structures.
