a9794e407c72bad81f9d5e67f936c8180955a455
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-07-01 15:09:36 -0700

    [test] added string compression tests to repo.

---
+++ es6/module_1/string_compression.js
@@ -0,0 +1,10 @@
+//Compress a string by creating a method that returns a compressed form the of the given string. 
+//If the given string is longer after the compression is attempted, return the original string instead.
+//Example: "aaabbcccc".stringCompression(); -> "a3b2c4" 
+//Example: "a".stringCompression(); -> "a" would be returned, not "a1"
+
+//Writing a traditional function declaration here is necessary
+//In Babel 'this' is not attached to window and will transpile into undefined!
+String.prototype.stringCompression = function() {
+
+} 
