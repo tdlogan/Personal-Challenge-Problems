25736dba2fa5f9b019fdb4b7e3045784f6b59441
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-07-01 11:45:24 -0700

    [solution] solution added for string permutation.

--- es5/module_1/string_permutations.js
+++ es5/module_1/string_permutations.js
@@ -2,12 +2,76 @@
 
 //Writing a traditional function declaration here is necessary
 //In Babel this is not attached to window and will transpile into undefined!
-"use strict";
+'use strict';
 
 String.prototype.permutationCheck = function (str) {
-  var characters = {};
 
   if (this.length !== str.length) {
     return false;
   }
+
+  var characters = {};
+  var letters = str.split('');
+
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = letters[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var i = _step.value;
+
+      if (!characters[i]) {
+        characters[i] = 1;
+      } else {
+        characters[i]++;
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator['return']) {
+        _iterator['return']();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = letters[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var j = _step2.value;
+
+      if (characters[j] === undefined) {
+        return false;
+      } else {
+        if (characters[j] === 0) {
+          delete characters[j];
+        }
+      }
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+        _iterator2['return']();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+
+  return true;
 };
