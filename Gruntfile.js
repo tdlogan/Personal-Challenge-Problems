ea71fb9a1c8eb603d572be289f00fe5b622ca513
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-07-01 13:53:28 -0700

    wiped out solutions from master.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,21 +10,13 @@ module.exports = function(grunt) {
         },
         dist: {
           src: ['es6/**/*.js'],
-<<<<<<< HEAD
-          dest: 'dist/<%= pkg.name %>.js'
-=======
           dest: 'es5/<%= pkg.name %>.js'
->>>>>>> solutions
         }
       },
       babel: {
         dist: {
           files: {
-<<<<<<< HEAD
-            'dist/transpiled.js' : 'dist/ES6_Challenge_Problems.js'
-=======
             'es5/transpiled.js' : 'es5/ES6_Challenge_Problems.js'
->>>>>>> solutions
           }
         }
       }
--- es5/ES6_Challenge_Problems.js
+++ es5/ES6_Challenge_Problems.js
@@ -4,62 +4,14 @@
 //In Babel this is not attached to window and will transpile into undefined!
 String.prototype.permutationCheck = function(str) {
 
-  if (this.length !== str.length) {
-    return false;
-  }
-
-  let characters = {};
-  let letters = str.split('');
-
-  for(let i of letters) {
-    if (!characters[i]) {
-      characters[i] = 1;
-    } else {
-      characters[i]++;      
-    }
-  }
-
-  for (let j of letters) {
-    if (characters[j] === undefined) {
-      return false;
-    } else {
-      if (characters[j] === 0) {
-        delete characters[j];
-      }
-    }
-  }
-  return true;
-<<<<<<< HEAD
-} ;//Create a function to determine if a string has all unique characters
-
-let unique = (str) => {
-
-}
-
-//EXTRA CREDIT: Implement the unique problem without creating additioanl data structures.
-=======
 } 
 ;//Create a function to determine if a string has all unique characters
 
 let unique = (str) => {
-  //Store of unique characters
-  let characters = {};
 
-  //Split the string into an array in order to avoid problems with unicode characters that have multiple indices
-  let letters = str.split('');
-
-  for (let i of letters) {
-    if (characters[i]) {
-      return false;
-    } else {
-      characters[i] = true;
-    }
-  }
-  return true;
 }
 
 //EXTRA CREDIT: Implement the unique problem without creating additioanl data structures
->>>>>>> solutions
 
 let unique_extra = (str) => {
 
