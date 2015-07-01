1a55ae38a27c93274373589cdc5b2047754583f5
Thomas Logan <thomaslogan@Thomass-MacBook-Pro-2.local>
2015-06-30 19:31:32 -0700

    working on string_permutation solution. Added new test.

--- es5/module_1/string_permutations.js
+++ es5/module_1/string_permutations.js
@@ -1,5 +1,13 @@
 //Given two strings, implement a method that determines if the input string is a permutation of the other
 
+//Writing a traditional function declaration here is necessary
+//In Babel this is not attached to window and will transpile into undefined!
 "use strict";
 
-String.prototype.permutationCheck = function (str1) {};
+String.prototype.permutationCheck = function (str) {
+  var characters = {};
+
+  if (this.length !== str.length) {
+    return false;
+  }
+};
--- test/string_permutations_test.js
+++ test/string_permutations_test.js
@@ -1,13 +1,18 @@
 describe('String Permutations: determine if the input is a permutation of the base string.', function() {
 
   /*  Overwrite methods from superclass  */
+  var name = "tommy"
+
+  it ('should return false when strings are different lengths', function() {
+    expect(name.permutationCheck('_tommy')).to.equal(false);
+  });
 
   it('should return false for strings that aren\'t permutations of each other', function() {
-    expect(('tommy').permutationCheck('gerald')).to.equal(false);
+    expect(name.permutationCheck('gerald')).to.equal(false);
   });
 
   it('should return true for strings that are permutations of each other', function() {
-    expect(('zoe').permutationCheck('eoz')).to.equal(true);
+    expect(name.permutationCheck('ymmot')).to.equal(true);
   });
 
 
