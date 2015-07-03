71d75afe86b8f723680e55350fde7087b0091ce2
Thomas Logan <tdloganj@gmail.com>
2015-07-03 16:30:17 -0700

    updating rotation tests.

--- test/string_rotation_test.js
+++ test/string_rotation_test.js
@@ -1,11 +1,20 @@
 describe('String Rotation: check to see if a string is a rotated version of another string.', function() {
 
-  it ('should compress string', function() {
+  it ('should return true for a rotated string', function() {
     expect(stringRotation('thomas', 'omasth')).to.equal(true);
+    expect(stringRotation('aabbcc', 'bccaab')).to.equal(true);
+    expect(stringRotation('aabbccbbaabbcc', 'baabbccaabbccb')).to.equal(true);
   });
 
-  it('should return original string if compressed version is longer', function() {
+  it('should return false if the strings are not rotated', function() {
     expect(stringRotation('thomas', 'tomhas')).to.equal(false);
   });
 
+  it('should return false if the strings are not the same length', function() {
+    expect(stringRotation('thomas', 'tmhas')).to.equal(false);
+  });
+
+  it('should return true if the strings same', function() {
+    expect(stringRotation('thomas', 'thomas')).to.equal(true);
+  });
 });
