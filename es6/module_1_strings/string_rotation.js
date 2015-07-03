de28f9c19c6dfc9692d4506256f868586e38fb47
Thomas Logan <tdloganj@gmail.com>
2015-07-03 16:46:30 -0700

    edited string rotation rules.

--- es6/module_1_strings/string_rotation.js
+++ es6/module_1_strings/string_rotation.js
@@ -1,5 +1,4 @@
 //In this problem we want to check to see if two strings are rotations of each other
-//You may only use one call to the given method .substring()
 
 //Example: stringRotation("thomas", "omasth"); -> true
 //Example: stringRotation("thomas", "smotha");-> false
