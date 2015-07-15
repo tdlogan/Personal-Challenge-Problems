b7d414414748c1742dafe4dc088efa327fe26a9a
Janessa Link <janessa@gmail.com>
2015-07-15 16:18:38 -0700

    [problem] RockPaperScissors completed with tests

--- es6/assorted_problems/rockPaperScissors.js
+++ es6/assorted_problems/rockPaperScissors.js
@@ -18,38 +18,8 @@ Example:
 rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 
 */
-
-let rockPaperScissors = (rounds=3)=> {
-
-
-  const sampleThrows = ["rock", "paper", "scissors"];
-  let round = [];
-  let results = [];
-  let roundsToGo = rounds;
-
-
-  let recurse = (roundsToGo) => {
-
-    for(let i = 0; i < sampleThrows.length; i++){
-      if(round.length === rounds){      //This round has enough throws
-        results.push(round);
-        print("Round completed", round);
-        round = [];
-
-        if(results.length === rounds){  //All the rounds has been completed
-          print("All rounds completed: ", results);
-          return results;
-        }
-      }
-      else{
-        round.push(sampleThrows[i]);
-        print("Building round throw: ", round);
-      }
-      //recurse(roundsToGo-1);
-    }
-  }
-
-  recurse(rounds);
-
+let rockPaperScissors = (rounds) => {
 
 };
+
+
